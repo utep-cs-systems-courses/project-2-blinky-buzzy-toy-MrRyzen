db7c8acaae634d6430a843f8eb4e40dfec408858
Cristobal Medina <cristobalmd01@gmail.com>
2020-07-15 13:24:50 -0600

    Added all states finishing buzzer

--- project/stateMachines.c
+++ project/stateMachines.c
@@ -2,6 +2,7 @@
 #include "stateMachines.h"
 #include "switches.h"
 #include "led.h"
+#include "buzzer.h"
 #include "timerLib/libTimer.h"
 
 char toggle_led = 0;
@@ -23,21 +24,29 @@ void state_advance() {
     break;
   case 2:
     toggle_led = LED_RED;
+    if(sw2_state_down && sw3_state_down) {
+      state = 3;
+      state_advance();
+    }
     break;
   case 3:
     toggle_led = LED_GREEN | LED_RED;
+    if(sw1_state_down && sw2_state_down && sw3_state_down) {
+      state = 4;
+      state_advance();
+    }
     //buzz
     break;
   case 4:
-    //buzz song
+    buzzer_set_period(D);
     state = 0;
     enableWDTInterrupts();
   case 0:
-    toggle_led = (toggle_led & LED_RED) ? LED_GREEN : LED_RED;
     if(sw1_state_down) {
       state = 1;
       state_advance();
     }
+    toggle_led = (toggle_led & LED_RED) ? LED_GREEN : LED_RED;
     break;
   default: break;
   }
