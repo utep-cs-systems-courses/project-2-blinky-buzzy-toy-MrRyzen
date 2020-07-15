5467ee82c99cb69dbed322a19fc611c2295ccedd
Cristobal Medina <cristobalmd01@gmail.com>
2020-07-15 13:55:02 -0600

    small changes +added exit state

--- project/stateMachines.c
+++ project/stateMachines.c
@@ -13,9 +13,19 @@ void state_init() {
   led_update();
 }
 
+void reset_states() {
+  state = 0;
+  toggle_led = 0;
+  buzzer_set_period(0);
+  led_update();
+  state_advance();
+}
+
 void state_advance() {  
   switch (state) {
   case 1:
+    if(sw4_state_down)
+      reset_states();
     toggle_led = LED_GREEN;
     if(sw1_state_down && sw3_state_down) {
       state = 2;
@@ -23,6 +33,8 @@ void state_advance() {
     }
     break;
   case 2:
+    if(sw4_state_down)
+      reset_states();
     toggle_led = LED_RED;
     if(sw2_state_down && sw3_state_down) {
       state = 3;
@@ -30,17 +42,21 @@ void state_advance() {
     }
     break;
   case 3:
+    if(sw4_state_down)
+      reset_states();
     toggle_led = LED_GREEN | LED_RED;
     if(sw1_state_down && sw2_state_down && sw3_state_down) {
       state = 4;
       state_advance();
     }
-    //buzz
     break;
   case 4:
     play_song();
     state = 0;
+    state_advance();
   case 0:
+    if(sw4_state_down)
+      reset_states();
     if(sw1_state_down) {
       state = 1;
       state_advance();
